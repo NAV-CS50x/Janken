c0de127a644814cd9b6460ffe8edb98f442c32cf
NAV <NAV.CS50x@gmail.com>
2022-04-18 13:05:59 -0400

    Add comments to help identify ReferenceError

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-let options = ["rock", "paper", "scissors"];
+// Define options of rock, paper, scissors
+const options = ["rock", "paper", "scissors"];
+
+// Set
 let computerSelection = computerPlay();
 
+// Get random selection from options
 function computerPlay() {
   let result = options[Math.floor(Math.random() * options.length)];
   return result;
 }
 
+// Set
 let playerSelection = playerPlay();
 
+// Get valid selection from player
 function playerPlay() {
   while (true) {
     let result = prompt("Choose rock, paper, or scissors!");
@@ -19,8 +25,10 @@ function playerPlay() {
   return result.toLowerCase();
 }
 
+// Play a round player vs computer
 function playRound(playerSelection, computerSelection) {
 
 }
 
+// Display player and computer selections in console
 console.log(playerSelection, computerSelection);
